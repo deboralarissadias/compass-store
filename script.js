c960e3a259a86f7120d543e9aa9c729bc396c9d6
Débora Larissa <larissadias.life@gmail.com>
2024-08-10 19:58:33 -0300

    sessão header-bottom concluída

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 // Funcionalidade de fechar a div de promoção
 function closeHeaderTop() {
-    document.querySelector(".header-top").style.display = "none";
+  document.querySelector(".header-top").style.display = "none";
 }
 
+function menuHamburger() {
+  var navs = document.querySelectorAll(".navbar__items");
+
+  navs.forEach((nav) => nav.classList.toggle("navbar__toggleShow"));
+}
+
+document.querySelector(".navbar__link-toggle").addEventListener("click", menuHamburger);
+
+function openSearch() {
+    var searchContainer = document.querySelector(".search-container-mobile");
+    if (searchContainer.style.display === "flex") {
+        searchContainer.style.display = "none";
+    } else {
+        searchContainer.style.display = "flex";
+    }
+}
 
