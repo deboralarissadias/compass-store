ee1a237fc6671213f536110544aa596ef5b4854c
Débora Larissa <larissadias.life@gmail.com>
2024-08-11 06:34:48 -0300

    seção banner e brands web concluída

--- script.js
+++ script.js
@@ -20,3 +20,26 @@ function openSearch() {
     }
 }
 
+
+function animateValue(id, start, end, duration) {
+    let range = end - start;
+    let current = start;
+    let increment = end > start ? 1 : -1;
+    let stepTime = Math.abs(Math.floor(duration / range));
+    let obj = document.getElementById(id);
+    
+    let timer = setInterval(function() {
+        current += increment;
+        obj.textContent = current + "+";
+        if (current == end) {
+            clearInterval(timer);
+        }
+    }, stepTime);
+}
+
+window.onload = function() {
+    animateValue("stat1", 0, 200, 2000);   
+    animateValue("stat2", 0, 2000, 2000);  
+    animateValue("stat3", 0, 30000, 2000);
+};
+
